52eb3cad41ae220a3e40bc9bffcdee2a6789ec4f
allelos <p.paliagkas@gmail.com>
2020-12-04 19:05:58 +0200

    load snipcart asynchronously

--- components/Snipcart/index.js
+++ components/Snipcart/index.js
@@ -14,7 +14,7 @@ const SnipCart = () => {
           onLoad="this.onload=null;this.rel='stylesheet'"
         />
       </Head>
-      <script defer src="https://cdn.snipcart.com/themes/v3.0.25/default/snipcart.js" />
+      <script async src="https://cdn.snipcart.com/themes/v3.0.25/default/snipcart.js" />
       <div id="snipcart" data-api-key="MmM3MzlkMGYtZWVlMy00MTBhLWE4NmEtZGRiZjIxYmZkNDdlNjM3MDgyMTc4ODYwNDM5MzY2" />
     </React.Fragment>
   );
